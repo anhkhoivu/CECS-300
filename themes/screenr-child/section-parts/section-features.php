2a653a9248e1828f00660638fd74f3f89a594995
anhkhoivu <anhkhoivu95@gmail.com>
2016-11-06 11:36:54 -0800

    Changed some stuff to my repo

--- themes/screenr-child/section-parts/section-features.php
+++ themes/screenr-child/section-parts/section-features.php
@@ -69,9 +69,9 @@ if ( ! screenr_is_selective_refresh() ) {
                     $items_alt_text = array("Lipreaders miss out on up to ninety percent of the conversation.",
                             "One third of Deaf kids cannot communicate with their family.",
                             "Atleast ninety five percent of Deaf children are born to hearing parents.");
-                    $items_post_links = array("/?p=81",
-                            "/?p=78",
-                            "/?p=75");
+                    $items_post_links = array("/97",
+                            "/96",
+                            "/95");
                     echo '<a href="'.$items_post_links[$count-1].'" alt="'.$items_alt_text[$count-1].'"><img src="/wp-content/uploads/2016/10/item'.$count.'.png" alt=""></a>';
                     switch ($item['thumb_type']) {
                         case 'icon':
